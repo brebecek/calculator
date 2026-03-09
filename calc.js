ccb5e141243e57a97114beb52c56bf36dead1e07
brebecek <178239305+brebecek@users.noreply.github.com>
2026-03-09 16:25:42 -0500

    Add functions for opertations and display
    Add functions for operations, and edit functions for better display management

--- calc.js
+++ calc.js
@@ -1,42 +1,59 @@
-const numA = 0;
-const numB = 0;
-const oper = "";
-
-function add(a,b){
-    return a + b;
-}
-
-function subtract(a,b){
-    return a - b;
-}
-
-function multiply(a,b){
-    return a * b;
-}
-
-function divide(a,b){
-    return a / b;
-}
+let currentInput = '';
+let currentOperation = '';
+let previousInput = '';
 
 function operate (){
-    dis[;ay]
-    if (oper === "+"){
-    return add(a,b);
-    } else if (oper === "-"){
-        return subtract(a,b);
-    } else if (oper === "*"){
-        return multiply(a,b);
-    } else if (oper === "/"){
-        return divide(a,b);
+    if (previousInput === '' || currentInput === '') return;
+    let result;
+    let prev = parseFloat(previousInput);
+    let current = parseFloat(currentInput);
+
+    switch (currentOperation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            if (current === 0){
+                alert("Indivisible");
+                return;
+            }
+            result = prev / current;
+            break;
+        default:
+            return;
+
     }
+    currentInput = result.toString();
+    currentOperation = '';
+    previousInput = '';
+    document.getElementById('display').value = currentInput;
 }
 
-const display = document.getElementById("display");
-
 function appendToDisplay(input){
-    display.value += input;
+    currentInput += input;
+    document.getElementById('display').value = `${previousInput} ${currentOperation} ${currentInput}`;
+    }
+
+function appendOperationToDisplay(operation) {
+    if(currentInput === '') return;
+    if(currentInput !== '') {
+        operate();
+    }
+    currentOperation = operation;
+    previousInput = currentInput;
+    currentInput = '';
+    document.getElementById('display').value = `${previousInput} ${currentOperation}`;
 }
 
 function clearDisplay(){
-    display.value = "";
+    currentInput = '';
+    previousInput = '';
+    currentOperation = '';
+    document.getElementById('display').value = '';
 }
